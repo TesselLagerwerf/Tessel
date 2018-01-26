3290bf5f86285a260776550ce8224df5f8b57079
TesselLagerwerf <t.h.lagerwerf@student.rug.nl>
2018-01-26 15:31:51 +0100

    adding a 1

---
+++ comitter.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+import os
+
+
+EmptyFile = open("counter", 'a')
+
+num = range(0,3)
+
+for i in num:
+	EmptyFile.write("1")
+	os.system('git add --all :/')
+	os.system('git commit -m "adding a 1"')
+	os.system('git push')
+EmptyFile.close()
+
+print "Done!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
