b8f7ce3602016111ebca9a9a22f399b7537de05d
TesselLagerwerf <t.h.lagerwerf@student.rug.nl>
2018-01-26 16:34:02 +0100

    Adding a 1

--- comitter.py
+++ comitter.py
@@ -2,18 +2,14 @@
 
 import os
 
-EmptyFile = open("counter", 'a')
-
 num = range(0,5)
 
-os.system('git pull')
-
 for i in num:
-	EmptyFile.write("1")
+	open("counter", 'a').write("1")
 	os.system('git add --all :/')
 	os.system('git commit -m "Adding a 1"')
 	os.system('git push')
-	os.system('git config --global push.default matching')
+
 EmptyFile.close()
 
 print "Done!"
